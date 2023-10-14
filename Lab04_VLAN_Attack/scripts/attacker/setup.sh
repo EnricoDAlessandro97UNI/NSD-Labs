b9eaf74d9a7189cb7f6cc43f612ca3516ac910d7
Enrico D'Alessandro <enrico.dalessandro@alumni.uniroma2.eu>
2023-10-14 16:31:02 +0200

    updated scripts vlan attack

--- Lab04_VLAN_Attack/scripts/attacker/setup.sh
+++ Lab04_VLAN_Attack/scripts/attacker/setup.sh
@@ -7,4 +7,4 @@ ip link set eth0.1 up
 ip link add link eth0.1 name eth0.1.20 type vlan id 20
 ip link set eth0.1.20 up
 ip addr add 10.0.20.250/24 dev eth0.1.20
-arp -s 10.0.20.102 <client-MAC> -i eth0.1.20 #if mac not known use broadcast MAC
+arp -s 10.0.20.102 d0:d0:d0:d0:d0:d0 -i eth0.1.20 #if mac not known use broadcast MAC
--- Lab04_VLAN_Attack/scripts/client4/setup.sh
+++ Lab04_VLAN_Attack/scripts/client4/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ip link set eth0 up
+ip link set dev eth0 address d0:d0:d0:d0:d0:d0
 ip addr add 10.0.20.102/24 dev eth0
 ip route add default via 10.0.20.1
